fe368637ced29b67dbdbbf095595a2d356be5890
MayconSantos04 <gadsence47@gmail.com>
2025-04-17 16:15:42 -0300

    fix: correções de responsividade e bugs de interação nos links e cards

--- script.js
+++ script.js
@@ -4,3 +4,11 @@ navbarToggle.addEventListener('click', function () {
     navbarToggle.classList.toggle('collapsed');
 });
 
+document.querySelectorAll('.navbar-nav .nav-link').forEach(link => {
+    link.addEventListener('click', () => {
+        const navbarCollapse = document.querySelector('.navbar-collapse');
+        if (navbarCollapse.classList.contains('show')) {
+            new bootstrap.Collapse(navbarCollapse).toggle();
+        }
+    });
+});
