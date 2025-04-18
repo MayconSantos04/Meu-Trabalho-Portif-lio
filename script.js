ac72980bdff09ab0f45b8a9c475b7fb80b192242
MayconSantos04 <gadsence47@gmail.com>
2025-04-18 17:50:03 -0300

    feat: correção de bugs e adição de um uma função onde retorna ao começo, junto a isso varias mudanças e melhorias no código

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-const navbarToggle = document.getElementById('navbarToggle');
-
-navbarToggle.addEventListener('click', function () {
-    navbarToggle.classList.toggle('collapsed');
-});
-
 document.querySelectorAll('.navbar-nav .nav-link').forEach(link => {
     link.addEventListener('click', () => {
         const navbarCollapse = document.querySelector('.navbar-collapse');
